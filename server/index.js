876a016eb1e58ee9a41eb0a8316f15cb0f8eeed7
Marcelo Martins <m.a.martins@live.com>
2025-12-06 23:46:52 -0300

    Configuração de deploy para Heroku

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { poolPromise } = require('./db');
 const incidentRoutes = require('./routes/incidents');
 const optionsRoutes = require('./routes/options');
 const cargoRoutes = require('./routes/cargo');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+require("dotenv").config();
 
 // Middleware
 app.use(cors());
@@ -26,6 +27,19 @@ app.get('/', (req, res) => {
     res.send('Incident Management API is running');
 });
 
+// Serve os arquivos estáticos do React após a build
+// Verifica se estamos em produção para servir a pasta correta
+if (process.env.NODE_ENV === "production") {
+    // Aponta para a pasta dist criada pelo Vite dentro de client
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    // Qualquer rota que não seja API será redirecionada para o index.html do React
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
